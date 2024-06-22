784af85a56c03cbbc0fa085e1faac981724e149f
Gökberk <gokberkavci95@gmail.com>
2024-06-22 12:51:46 +0300

    commit

---
+++ AngularProject/AngularProject/AngularProjectContext/Context.cs
@@ -0,0 +1,15 @@
+﻿using AngularProject.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularProject.AngularProjectContext
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
